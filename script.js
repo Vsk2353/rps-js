af91e3093d59ad6e1245eeb3876dcfb327c5b8b2
Visky <65082238+Vsk2353@users.noreply.github.com>
2025-03-03 14:15:23 +0530

    Implement user choice input function

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     let a = Math.floor(Math.random()*3)
     
     if (a === 0) {return "rock";}
     else if (a === 1) {return "paper";}
     else {return ("scissors")};
+}
+
+function getUserChoice(){
+    let userChoice = prompt("Enter 'rock', 'paper' or 'scissors'","");
+    return userChoice.toLowerCase();
+}
+
+function playRound(userChoice,computerChoice){
+    if (userChoice === computerChoice){return ""}
 }
